121a0145d88f393345c9aba5fb16aa6eb0d5f206
Salah Eddine ZEMMOURI <zemm.salaheddine@gmail.com>
2021-09-19 02:07:36 +0100

    added buildGrid method

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { roadmap } from "./assets/roadmap";
-import { parse } from "./Roadmap/Roadmap.helper";
+import { buildGrid } from "./Roadmap/Roadmap.helper";
+
 
 const App = () => {
-  const graph = parse(roadmap);
-
-  return <div>Hello World</div>;
+  return buildGrid(roadmap);
 };
 
 export default App;
--- src/Roadmap/Roadmap.helper.js
+++ src/Roadmap/Roadmap.helper.js
@@ -1,17 +1,42 @@
-export const parse = ({ entryLevel, levels }) => {
+export const parse = ({ entryLevel, levels, rows, cols }) => {
   const nodeIndex = {};
   const nodes = [];
+  const gridNodes = Array.from({ length: rows }, () => (
+    Array.from({ length: cols })
+  ))
 
   Object.entries(levels).forEach(([id, level]) => {
     const node = { ...level, id };
-
     nodes.push(node);
     nodeIndex[id] = node;
+    gridNodes[level.row][level.col] = node;
   });
 
   Object.entries(levels).forEach(([id, { next }]) => {
     nodeIndex[id].next = next.map((nextId) => nodeIndex[nextId]);
   });
 
-  return nodeIndex[entryLevel];
+  return gridNodes;
 };
+
+export const buildGrid = (roadmap) => {
+  const gridNodes = parse(roadmap);
+
+  return (
+    <table>
+      <tbody>
+        {
+          Array.from({ length: roadmap.rows }, (_, row) => (
+            <tr>
+              {Array.from({ length: roadmap.cols }, (_, col) => (
+                <td>{
+                  gridNodes[row][col] && gridNodes[row][col].name
+                }</td>
+              ))}
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>)
+
+}
